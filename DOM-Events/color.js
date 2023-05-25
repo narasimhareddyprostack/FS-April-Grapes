68ef488b107cce8fda8d5f70e3b2be4fdb22f34d
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2023-05-25 10:57:05 +0530

    okie

---
+++ DOM-Events/color.js
@@ -0,0 +1,15 @@
+function cc_MouseOver() {
+    document.getElementById('mover').style.backgroundColor = "Yellow"
+    //alert("Test case 123")
+}
+
+function cc_MouseOut() {
+    document.getElementById('mout').style.backgroundColor = "pink"
+}
+let cc_Click = () => {
+    document.getElementById('click').style.backgroundColor = "red"
+}
+
+function cc_Focus() {
+    document.getElementById('focusid').style.backgroundColor = "Yellow"
+}
---
+++ FatArrow/one.js
@@ -0,0 +1,12 @@
+/* function add() {
+    console.log("Additon")
+}
+
+add()
+add() */
+
+let add = () => {
+    console.log("Addition")
+}
+
+add()
